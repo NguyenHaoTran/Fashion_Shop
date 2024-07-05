f83aa8906775aa47378a4b7dcf831c36b5fc24a9
NguyenHaoTran <henryporison@outlook.com.vn>
2024-07-05 22:27:01 +0700

    fixedanv

--- src/components/header/black_line/BlackLine.jsx
+++ src/components/header/black_line/BlackLine.jsx
@@ -1,23 +1,27 @@
-import './blackLine.scss'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import "./blackLine.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const BlackLine = () => {
   return (
     <div className="container">
-        <div className="account">
-            <a href="#">Tài khoản</a>
-        </div>
-        <div className="text">
-            <p>Chương trình giảm giá theo mùa đã bắt đầu! Giảm tới 50%! Mua ngay</p>
-        </div>
-        <div className="language">
+      <div className="account">
+        <a href="#">Tài khoản</a>
+      </div>
+      <div className="text">
+        <p>Chương trình giảm giá theo mùa đã bắt đầu! Giảm tới 50%! Mua ngay</p>
+      </div>
+      <div className="language">
+        <div className="items">
+          <div className="vn">
             <p>Việt Nam</p>
             <span className="lg-span">
-              <KeyboardArrowDownIcon/>
+              <KeyboardArrowDownIcon />
             </span>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlackLine
+export default BlackLine;
