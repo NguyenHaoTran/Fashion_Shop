59d9769ccfca47638448b16503281bb161a30260
NguyenHaoTran <henryporison@outlook.com.vn>
2024-07-06 10:37:07 +0700

    uploadcode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,26 @@
-import "./header.scss"
-import Nav from "./nav/Nav"
-import Search from './search/Search'
-import Cart from "./cart/Cart"
+import "./header.scss";
+import Nav from "./nav/Nav";
+import Search from "./search/Search";
+import Cart from "./cart/Cart";
 
-import Wishlist from '../../pages/wishlist/Wishlist'
-import BlackLine from "./black_line/BlackLine"
+import Wishlist from "../../pages/wishlist/Wishlist";
+import BlackLine from "./black_line/BlackLine";
 
 const Header = () => {
   return (
     <>
-    <BlackLine/>
-    <header>
+      <BlackLine />
+      <header>
         <div className="logo">LOGO</div>
-          <Nav/>
-          <div className="tool">
-            <Search/>
-            <Wishlist/>
-            <Cart/>
-        </div>       
-    </header>
+        <Nav />
+        <div className="tool">
+          <Search />
+          <Wishlist />
+          <Cart />
+        </div>
+      </header>
     </>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,33 @@
 import "./home.scss";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="home">
+        <div className="content">
+          <div className="items">
+            <div className="text">
+              <h1>
+                Đón xuân <br /> COLLECTION
+              </h1>
+              <button>
+                Đến ngay
+                <span>
+                  <ArrowForwardIcon />
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className="img">
+            <img src="/1x1_menu.webp" />
+          </div>
+        </div>
+        <div className="year">2024</div>
+      </div>
+      <div className="widget"></div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
