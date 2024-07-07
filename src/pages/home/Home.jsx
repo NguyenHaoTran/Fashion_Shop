e1d7eeb82550a68fdcc613f650acb11e9313f647
NguyenHaoTran <henryporison@outlook.com.vn>
2024-07-07 12:44:46 +0700

    updatecode_day3

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,9 @@ import SquareIcon from "@mui/icons-material/Square";
 import CircleIcon from "@mui/icons-material/Circle";
 import SquareOutlinedIcon from "@mui/icons-material/SquareOutlined";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import StarBorderPurple500Icon from "@mui/icons-material/StarBorderPurple500";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 import ScrollReveal from "scrollreveal";
 
@@ -114,7 +117,80 @@ const Home = () => {
             </p>
           </div>
         </div>
-        <div className="home_note"></div>
+        <div className="home_note">
+          <div className="chat_icon">
+            <QuestionAnswerIcon fontSize="500" />
+          </div>
+          <div className="stars">
+            <StarBorderPurple500Icon />
+            <StarBorderPurple500Icon />
+            <StarBorderPurple500Icon />
+            <StarBorderPurple500Icon />
+            <StarBorderPurple500Icon />
+          </div>
+          <div className="note">
+            <p>
+              TÔI LUÔN LUÔN TÌM THẤY VẺ ĐẸP TRONG NHỮNG THỨ KỲ LẠ VÀ KHÔNG HOÀN
+              HẢO. CHÚNG LUON THÚ VỊ HƠN NHIỀU
+            </p>
+          </div>
+          <div className="CEO_name">NGUYỄN HÙNG</div>
+          <div className="sub_name">Founder & CEO</div>
+          <div className="note_imgs">
+            <img src="/900x420_img_2.avif" alt="" />
+            <p>COLLECTION</p>
+            <b>2025</b>
+          </div>
+          <div className="brands">
+            <img src="/logoBrands/Alexander-McQueen.png" alt="" />
+            <img src="/logoBrands/Balmain Paris.png" alt="" />
+            <img src="/logoBrands/Burberry Group.png" alt="" />
+            <img src="/logoBrands/hermes.png" alt="" />
+            <img src="/logoBrands/prada.png" alt="" />
+          </div>
+        </div>
+        <div className="instagram">
+          <div className="box">
+            <div className="ig_logo">
+              <InstagramIcon  />
+            </div>
+            <div className="ig_img">
+              <img src="/ig_imgs/ig_img_1.webp" alt="" />
+            </div>
+          </div>
+          <div className="box">
+            <div className="ig_logo">
+              <InstagramIcon  />
+            </div>
+            <div className="ig_img">
+              <img src="/ig_imgs/ig_img_2.webp" alt="" />
+            </div>
+          </div>
+          <div className="box">
+            <div className="ig_logo">
+              <InstagramIcon  />
+            </div>
+            <div className="ig_img">
+              <img src="/ig_imgs/ig_img_3.webp" alt="" />
+            </div>
+          </div>
+          <div className="box">
+            <div className="ig_logo">
+              <InstagramIcon  />
+            </div>
+            <div className="ig_img">
+              <img src="/ig_imgs/ig_img_4.webp" alt="" />
+            </div>
+          </div>
+          <div className="box">
+            <div className="ig_logo">
+              <InstagramIcon  />
+            </div>
+            <div className="ig_img">
+              <img src="/ig_imgs/ig_img_5.webp" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
