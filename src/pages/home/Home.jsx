f54e977051f3fa9138410446d56aa50568f8bd91
NguyenHaoTran <henryporison@outlook.com.vn>
2024-07-07 09:27:07 +0700

    uploadday3

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,11 @@
 import "./home.scss";
 import ProductsList from "../products/ProductsList";
+
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import SquareIcon from "@mui/icons-material/Square";
+import CircleIcon from "@mui/icons-material/Circle";
+import SquareOutlinedIcon from "@mui/icons-material/SquareOutlined";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 import ScrollReveal from "scrollreveal";
 
@@ -13,8 +18,8 @@ const Home = () => {
     delay: 500,
   });
 
-  sr.reveal(`.text`, {origin: 'left'})
-  sr.reveal(`.img`, {origin: 'right'})
+  sr.reveal(`.text`, { origin: "left" });
+  sr.reveal(`.img`, { origin: "right" });
 
   return (
     <>
@@ -66,12 +71,50 @@ const Home = () => {
           </p>
         </div>
         <div className="home_list_products">
-            <ProductsList/>
+          <ProductsList />
         </div>
       </div>
       {/* Home_about */}
       <div className="home_about">
-        
+        <div className="title">
+          <h5>VỀ LOGO</h5>
+          <p>Đón đầu xu hướng, bức phá khỏi vùng an toàn</p>
+        </div>
+        <div className="about_items">
+          <div className="item">
+            <SquareIcon color="black" className="icon rotate" />
+            <b>TOP TRENDING</b>
+            <p>
+              Thời trang có thể phai tàn nhưng phong cách sẽ tồn tại mãi mãi. Để
+              không ai có thể thay thế, bạn phải luôn luôn khác biệt
+            </p>
+          </div>
+          <div className="item">
+            <CircleIcon color="black" className="icon" />
+            <b>SẢN PHẨM CHẤT LƯỢNG</b>
+            <p>
+              Thời trang có thể phai tàn nhưng phong cách sẽ tồn tại mãi mãi. Để
+              không ai có thể thay thế, bạn phải luôn luôn khác biệt
+            </p>
+          </div>
+          <div className="item">
+            <SquareOutlinedIcon color="black" className="icon" />
+            <b>MIỄN PHÍ VẬN CHUYỂN</b>
+            <p>
+              Thời trang có thể phai tàn nhưng phong cách sẽ tồn tại mãi mãi. Để
+              không ai có thể thay thế, bạn phải luôn luôn khác biệt
+            </p>
+          </div>
+          <div className="item">
+            <Brightness7Icon color="black" className="icon" />
+            <b>HỖ TRỢ ONLINE</b>
+            <p>
+              Thời trang có thể phai tàn nhưng phong cách sẽ tồn tại mãi mãi. Để
+              không ai có thể thay thế, bạn phải luôn luôn khác biệt
+            </p>
+          </div>
+        </div>
+        <div className="home_note"></div>
       </div>
     </>
   );
