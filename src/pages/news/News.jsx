b19e5eb70ed3f2faf9c9db20eb145ea3f649769a
NguyenHaoTran <henryporison@outlook.com.vn>
2024-07-27 07:25:45 +0700

    news

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,7 +1,19 @@
 import "./news.scss";
 
-function News() {
-  return <div>News</div>;
+
+export const News = () => {
+  return (
+    <div className="News">
+      <div className="banner">
+        <div className="selection">
+          <a href="/">TRANG CHỦ</a>
+          <span>|</span>
+          <a href="/products">TIN TỨC</a>
+        </div>
+        <p className="p_banner">TIN TỨC</p>
+      </div>
+    </div>
+  )
 }
 
 export default News;
